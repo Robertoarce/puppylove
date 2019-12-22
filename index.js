972e1dcbb7cacd6368169c0f428aeff35ae912de
Titorium <roberto_arce_@hotmail.com>
2019-12-22 15:13:44 +0100

    adding procfile, package

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const express = require('express')
 const app = express()
 
 // just a constant for the port
-const port = 3000
+const port = 3000;
 
 //  when it gets ("/") it sends the response 'hello world'), two ways: 
 app.get('/', (req, res) => res.send('Hello There!'))
 app.get('/', function(req, res){res.send('Hello There!')});
 
 // listen to the port: "port", when happens do this : "console..."
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+app.listen(process.env.PORT);
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
