6402809f1a54188ccf2b2d608f0ce38a2dcfdd11
Titorium <roberto_arce_@hotmail.com>
2019-12-22 18:48:50 +0100

    adding ejs

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
+
+
+//---------------- Libraries ----------------
 // get the module
 const express = require('express')
+
+
+//---------------- constructors ----------------
 
 //contructor
 const app = express()
 
-// just a constant for the port
-const port = 3000;
+
+//---------------- Pages ----------------
 
 //  when it gets ("/") it sends the response 'hello world'), two ways: 
-app.get('/', (req, res) => res.send('Hello There!'))
+
+// Home Page
 app.get('/', function(req, res){res.send('Hello There!')});
 
-// listen to the port: "port", when happens do this : "console..."
 
 
-
+//---------------- ports ----------------
 app.listen(process.env.PORT);
+
+// const port = 3000;
 // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
