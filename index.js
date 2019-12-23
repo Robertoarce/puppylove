7e2a58dea53d69a8da3b3f165cda708d7dba5c3c
Titorium <roberto_arce_@hotmail.com>
2019-12-23 18:17:35 +0100

    adding ejs files and images

--- index.js
+++ index.js
@@ -2,26 +2,31 @@
 
 //---------------- Libraries ----------------
 // get the module
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+
 
 
 //---------------- constructors ----------------
 
 //contructor
 const app = express()
+app.set("view engine", "ejs");
 
+//---------------- adding body parser and CSS  ----------------
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 //---------------- Pages ----------------
-
-//  when it gets ("/") it sends the response 'hello world'), two ways: 
+ 
 
 // Home Page
-app.get('/', function(req, res){res.send('Coucou Bolosse :) !')});
-
-
+app.get('/', function(req, res){
+    res.render("home");});
+ 
 
 //---------------- ports ----------------
-app.listen(process.env.PORT );
+app.listen(process.env.PORT || 3000);
 
 // const port = 3000;
 // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
