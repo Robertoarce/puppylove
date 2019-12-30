14c8ee1ae980089bfe2ae4ea986207d494b64009
Titorium <roberto_arce_@hotmail.com>
2019-12-30 19:17:07 +0100

    friend forever

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 
-
 //---------------- constructors ----------------
 
 //contructor
@@ -15,8 +14,8 @@ app.set("view engine", "ejs");
 
 //---------------- adding body parser and CSS  ----------------
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
-
+// app.use(express.static("public"));
+app.use('/public', express.static(__dirname + "/public"));
 //---------------- Pages ----------------
  
 
@@ -24,6 +23,23 @@ app.use(express.static("public"));
 app.get('/', function(req, res){
     res.render("home");});
  
+// Client Page
+app.get('/clients', function(req, res){
+    res.render("clients");});
+
+ // Vets Page
+app.get('/vets', function(req, res){
+    res.render("vets");});
+ // Client Page
+ 
+app.get('/shop', function(req, res){
+    res.render("shop");});
+
+ // Client Page
+app.get('/findbud', function(req, res){
+    res.render("findbud");});
+ 
+
 
 //---------------- ports ----------------
 app.listen(process.env.PORT || 3000);
